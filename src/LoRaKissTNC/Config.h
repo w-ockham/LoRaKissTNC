234048de2a9bac9b78a4a73e83f3e953dfcad815
Minoru Tomobe <w-ockham@users.noreply.github.com>
2020-01-25 13:28:34 +0900

    Initial commit.

---
+++ src/LoRaKissTNC/Config.h
@@ -0,0 +1,69 @@
+
+#ifndef CONFIG_H
+  #define CONFIG_H
+
+  #define MAJ_VERS 0x01
+  #define MIN_VERS 0x00
+
+  #define MCU_328P 0x90
+  #define MCU_32U4 0x91
+
+  #if defined(__AVR_ATmega328P__)
+    #define MCU_VARIANT MCU_328P
+    #warning "Firmware is being compiled for ATmega328p based boards"
+  #elif defined(__AVR_ATmega32U4__)
+    #define MCU_VARIANT MCU_32U4
+    #warning "Firmware is being compiled for ATmega32u4 based boards"
+  #else
+    #error "The firmware cannot be compiled for the selected MCU variant"
+  #endif
+  
+  #define MTU  255
+  #define CMD_L       4
+  int     lastRssi = 0;
+  float   lastSnr  = 0;
+  uint8_t lastRssiRaw = 0x00;
+  
+  size_t  readLength = 0;
+
+  #if MCU_VARIANT == MCU_328P
+    const int pinNSS = 10;
+    const int pinNRST = 9;
+    const int pinDIO0 = 2;
+    // const int pinLedRx = 5;
+    // const int pinLedTx = 4;
+
+  #endif
+
+  #if MCU_VARIANT == MCU_32U4
+    const int pinNSS  = 8;
+    const int pinNRST = 4;
+    const int pinDIO0 = 7;
+  #endif
+
+  const long serialBaudRate   = 9600;
+  
+  int  loraMaxBackoff = 8000;
+  uint32_t backofft;
+  uint32_t backoffDuration;
+  bool isBackoff = false;
+  
+  // Default LoRa settings
+  int       loraSpreadingFactor = 10;
+  int       loraCodingRate      = 8;
+  int       loraTxPower         = 20;
+  uint32_t  bandWidthTable[] = { 7.8e3, 10.4e3, 15.6e3, 20.8e3, 31.25e3, 41.7e3, 62.5e3, 125e3, 250e3};
+  uint32_t  loraBandwidth       = 6;
+  const uint32_t freq_low  = 431.0E6;
+  const uint32_t freq_high = 439.0E6;
+  uint32_t  loraFrequency       = 438.41E6;
+
+  uint8_t txBuffer[MTU];
+  uint8_t rxBuffer[MTU];
+
+  uint32_t statRx = 0;
+  uint32_t statTx = 0;
+
+  bool outboundReady = false;
+
+#endif
---
+++ src/LoRaKissTNC/LoRaKissTNC.ino
@@ -0,0 +1,268 @@
+#include <LoRa.h>
+#include "Config.h"
+#include "KISS.h"
+bool commandMode = true;
+char line_buffer[128];
+int bufc = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(serialBaudRate);
+  while (!Serial); // Waiting until LoRa32u4 is ready
+
+  // Buffers
+  memset(rxBuffer, 0, sizeof(rxBuffer));
+  memset(txBuffer, 0, sizeof(txBuffer));
+
+  LoRa.setPins(pinNSS, pinNRST, pinDIO0);
+
+  startRadio();
+}
+
+bool startRadio() {
+  if (!LoRa.begin(loraFrequency)) {
+    kissIndicateError(ERROR_INITRADIO);
+    Serial.println("FAIL");
+    while(1);
+  }
+  else {
+    LoRa.setSpreadingFactor(loraSpreadingFactor);
+    LoRa.setCodingRate4(loraCodingRate);
+    LoRa.setSignalBandwidth(bandWidthTable[loraBandwidth]);
+    //LoRa.enableCrc();
+    LoRa.onReceive(receiveCallback);
+    LoRa.receive();
+  }
+}
+
+void transmit(size_t size) {
+  size_t written = 0;
+
+  if (size > MTU) {
+    size = MTU;
+  }
+
+  LoRa.beginPacket();
+  for (size_t i; i < size; i++) {
+    LoRa.write(txBuffer[i]);
+    written++;
+  }
+  LoRa.endPacket();
+  LoRa.receive();
+}
+
+void serialCallback(uint8_t txByte) {
+  if (inFrame && txByte == FEND && command == CMD_DATA) {
+    inFrame = false;
+    //Serial.println("FULL_KISS");
+    if (outboundReady) {
+        kissIndicateError(ERROR_QUEUE_FULL);
+    }
+    else {
+        outboundReady = true;
+        //Serial.println("RDY_OUT");
+    }
+  }
+  else if (txByte == FEND) {
+    //Serial.println("KISS_FLAG");
+    inFrame = true;
+    command = CMD_UNKNOWN;
+    frameLength = 0;
+  }
+  else if (inFrame && frameLength < MTU) {
+    // Get command byte
+    if (frameLength == 0 && command == CMD_UNKNOWN) {
+      //Serial.println("ACQ_CMD");
+      command = txByte;
+    }
+    else if (command == CMD_DATA) {
+      if (txByte == FESC) {
+        escape = true;
+      }
+      else {
+        if (escape) {
+          if (txByte == TFEND) {
+            txByte = FEND;
+          }
+          if (txByte == TFESC) {
+            txByte = FESC;
+          }
+          escape = false;
+        }
+        else {
+          txBuffer[frameLength++] = txByte;
+        }
+      }
+    }
+  }
+}
+
+bool isOutboundReady() {
+  return outboundReady;
+}
+
+void receiveCallback(int packetSize) {
+  readLength = 0;
+  lastRssi = LoRa.packetRssi();
+  lastSnr = LoRa.packetSnr();
+  getPacketData(packetSize);
+
+  // Send RSSI
+  Serial.write(FEND);
+  Serial.write(CMD_HARDWARE);
+  Serial.print(lastRssi);
+  Serial.write(',');
+  Serial.print(lastSnr);
+  Serial.write(FEND);
+
+  // And then write the entire packet
+  Serial.write(FEND);
+  Serial.write((uint8_t)CMD_DATA);
+  for (int i = 0; i < readLength; i++) {
+    uint8_t temp = rxBuffer[i];
+    if (temp == FEND) {
+      Serial.write(FESC);
+      temp = TFEND;
+    }
+    if (temp == FESC) {
+      Serial.write(FESC);
+      temp = TFESC;
+    }
+    Serial.write(temp);
+  }
+  Serial.write(FEND);
+  readLength = 0;
+}
+
+void escapedSerialWrite (uint8_t bufferByte) {
+  switch(bufferByte) {
+    case FEND:
+      Serial.write(FESC);
+      Serial.write(TFEND);
+      break;
+    case FESC:
+      Serial.write(FESC);
+      Serial.write(TFESC);
+      break;
+    default:
+      Serial.write(bufferByte);
+  }
+}
+
+void kissIndicateError(uint8_t errorCode) {
+  Serial.write(FEND);
+  Serial.write(CMD_ERROR);
+  Serial.write(errorCode);
+  Serial.write(FEND);
+}
+
+void getPacketData(int packetLength) {
+  while (packetLength--) {
+    rxBuffer[readLength++] = LoRa.read();
+  }
+}
+
+void do_command(char buffer[]) {
+  char *cmd;
+  uint32_t f;
+  int bw,sf,cr,backoff;
+
+  cmd = strtok(buffer," ");
+  Serial.println(cmd);
+  if ( strcasecmp(cmd,"set") == 0) {
+    f = atol(strtok(NULL,","));
+    bw = atoi(strtok(NULL,","));
+    sf = atoi(strtok(NULL,","));
+    cr = atoi(strtok(NULL,","));
+    backoff = atoi(strtok(NULL,","));
+
+    Serial.println("LoRa Parameters:");
+
+    f = f * 10000;
+    Serial.print("Freq=");
+    Serial.println(f);
+    if (f < freq_low || f > freq_high) {
+      Serial.println("Out of band.");
+      return;
+    }
+    loraFrequency = f;
+    LoRa.setFrequency(loraFrequency);
+
+    Serial.print("BW=");
+    Serial.println(bw);
+    if (bw < 0 || bw > 8) {
+      Serial.println("unsupported bandwidth.");
+      return;
+    }
+    loraBandwidth = bw;
+    LoRa.setSignalBandwidth(bandWidthTable[loraBandwidth]);
+
+    Serial.print("SF=");
+    Serial.println(sf);
+    if (sf < 6 || sf > 12) {
+      Serial.println("unsupported spredingfactor.");
+      return;
+    }
+    loraSpreadingFactor = sf;
+    LoRa.setSpreadingFactor(loraSpreadingFactor);
+
+    Serial.print("CR=");
+    Serial.println(cr);
+    if (cr < 5 || cr > 8) {
+      Serial.println("unsupported codingrate.");
+      return;
+    }
+    loraCodingRate = cr;
+    LoRa.setCodingRate4(loraCodingRate);
+
+    Serial.print("Backoff=");
+    Serial.println(backoff);
+    if (backoff < 500) {
+      Serial.println("backtime error.");
+      return;
+    }
+    loraMaxBackoff = backoff;
+    Serial.println(F("TNC MODE."));
+    commandMode = false;
+  } else {
+    Serial.print(F("Unknown command:"));
+    Serial.println(buffer);
+  }
+}
+void loop() {
+   if (isOutboundReady() && !SERIAL_READING) {
+    if (!isBackoff) {
+      backofft = millis();
+      backoffDuration = random(500,loraMaxBackoff);
+      isBackoff = true;
+    } else if((millis() - backofft > backoffDuration)) {
+        isBackoff = false;
+        outboundReady = false;
+        transmit(frameLength);
+    }
+   }
+
+  if (Serial.available()) {
+    SERIAL_READING = true;
+    char txByte = Serial.read();
+    if (commandMode) {
+     if (txByte == '\r') {
+      line_buffer[bufc] = '\0';
+      do_command(line_buffer);
+      line_buffer[0] = '\0';
+      bufc = 0;
+     } else {
+      line_buffer[bufc++] =  txByte;
+      if (bufc > 128)
+       bufc = 127;
+     }
+    } else {
+      serialCallback(txByte);
+    }
+    lastSerialRead = millis();
+  } else {
+    if (SERIAL_READING && millis() - lastSerialRead >= serialReadTimeout) {
+      SERIAL_READING = false;
+    }
+  }
+}
